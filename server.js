f95a86aead6979ca822d73c1965a5f620ca268b8
Khadyr <zanderjvr@live.co.za>
2021-11-03 16:05:50 +0200

    Routes added and user page

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express")
+const router = require("./routes/users")
 const app = express()
 
 app.set('view engine', 'ejs')
@@ -7,5 +8,9 @@ app.get('/', (req, res) => {
     console.log("Here")
     res.render("index")
 })
+
+const userRouter = require('./routes/users')
+
+app.use('/users', userRouter)
 
 app.listen(3000)

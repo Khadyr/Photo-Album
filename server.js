77b05ff4fd0202c2cb152c01abe821d3071f0a16
Khadyr <zanderjvr@live.co.za>
2021-11-03 17:19:04 +0200

    Layout added

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 const express = require("express")
 const router = require("./routes/users")
 const app = express()
+const expressLayouts = require('express-ejs-layouts')
 
-app.set('view engine', 'ejs')
-
-app.get('/', (req, res) => {
-    console.log("Here")
-    res.render("index")
-})
-
+const indexRouter = require('./routes/index')
 const userRouter = require('./routes/users')
 
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views')
+app.set('layout', 'layouts/layout')
+app.use(expressLayouts)
+app.use(express.static('public'))
+
+app.use('/', indexRouter)
 app.use('/users', userRouter)
 
-app.listen(3000)
+app.listen(process.env.PORT || 3000) 

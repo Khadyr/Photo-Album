d69710c113cf032c1b8ca42ac96268a485c57d0c
Khadyr <zanderjvr@live.co.za>
2021-11-03 15:31:06 +0200

    view engine setup and index page view

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express")
 const app = express()
 
+app.set('view engine', 'ejs')
+
 app.get('/', (req, res) => {
     console.log("Here")
-    res.send("Hi")
+    res.render("index")
 })
 
 app.listen(3000)

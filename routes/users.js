829cca88b975a29d09117d1b4844b035426f96d7
Khadyr <zanderjvr@live.co.za>
2021-11-03 16:40:30 +0200

    User routes added

--- routes/users.js
+++ routes/users.js
@@ -9,4 +9,20 @@ router.get("/new", (req, res) =>{
     res.send("New User Form")
 })
 
+router.post('/', (req, res) => {
+    res.send("Create user")
+})
+
+router
+    .route("/:id")
+    .get((req, res) => {    
+        res.send(`Get user with id ${req.params.id}`)
+    })
+    .put((req, res) => {    
+        res.send(`Update user with id ${req.params.id}`)
+    })
+    .delete((req, res) => {    
+        res.send(`Delete user with id ${req.params.id}`)
+    })
+
 module.exports = router

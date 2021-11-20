8658f172e68fccf70bdaa45cd5f7a16e1a0ea6cf
Khadyr <zanderjvr@live.co.za>
2021-11-20 11:31:25 +0200

    Users routes added

--- routes/users.js
+++ routes/users.js
@@ -1,16 +1,47 @@
 const express = require("express")
 const router = express.Router()
+const User = require('../models/user')
 
-router.get("/", (req, res) => {
-    res.send("User List")
+// GET Users Route
+router.get("/", async (req, res) => {
+    let searchOptions = {}
+    if (req.query.name != null && req.query.name !== '') {
+        searchOptions.name = new RegExp(req.query.name, 'i')
+    }
+    try {
+        const users = await User.find(searchOptions)
+        res.render('users/index', { 
+            users: users, 
+            searchOptions: req.query
+        })
+    } catch (error) {
+        res.redirect('/')
+    }
+    
 })
 
+// New User Route
 router.get("/new", (req, res) =>{
-    res.send("New User Form")
+    res.render('users/new', { user: new User() })
 })
 
-router.post('/', (req, res) => {
-    res.send("Create user")
+//let locals = { errorMessage: 'Error creating User'}
+
+// Create User Route
+router.post('/', async (req, res) => {
+    const user = new User({
+        name: req.body.name
+    })
+    try {
+        const newUser = await user.save()
+        //res.redirect(`users/${newUser.id}`)
+        res.redirect(`users`)
+    } catch (error) {
+        res.render('users/new', {
+            user: user,
+            errorMessage: 'Error creating User'              
+        })
+    }     
 })
 
 router
